d5799318e81391ad3d469129adae37fdf2d87984
Chris Roat <1053153+chrisroat@users.noreply.github.com>
2021-06-04 13:55:59 +0800

    Add tests to demonstrate topological sort

--- test_pipeline.py
+++ test_pipeline.py
@@ -1,20 +1,60 @@
 import uuid
 
+import datajoint as dj
 import numpy as np
 import pytest
 
 import pipeline
 
 
+@pytest.fixture
+def schema_name():
+    return "test_" + uuid.uuid4().hex[:16]
+
+
 @pytest.fixture()
-def m():
-    schema_name = "test_" + uuid.uuid4().hex[:16]
+def schema_models(schema_name):
     schema, models = pipeline.create(schema_name)
-    yield models
+    yield schema, models
     schema.drop(force=True)
 
 
-def test_populate(m):
+def test_topological_sort(schema_models):
+    schema = schema_models[0]
+    tables = dj.Diagram(schema).topological_sort()
+    tables = [t.split(".")[1].strip("`") for t in tables]
+    tables = [t for t in tables if t.startswith("__")]
+    assert tables == [
+        "__deconvolve_start",
+        "__preprocess_start",
+        "__preprocess_done",
+        "__preprocess_done__part",
+        "__analyze",
+        "__deconvolve",
+        "__preprocess",
+    ]
+
+
+# Need schema_models to be sure that database was created.
+def test_topological_sort_virtual(schema_name, schema_models):
+    module = dj.create_virtual_module("schema.py", schema_name)
+    schema = getattr(module, "schema")
+    tables = dj.Diagram(schema).topological_sort()
+    tables = [t.split(".")[1].strip("`") for t in tables]
+    tables = [t for t in tables if t.startswith("__")]
+    assert tables == [
+        "__deconvolve_start",
+        "__preprocess_start",
+        "__preprocess_done",
+        "__preprocess_done__part",
+        "__analyze",
+        "__deconvolve",
+        "__preprocess",
+    ]
+
+
+def test_populate(schema_models):
+    m = schema_models[1]
     m.DeconvolveParams.insert1({"deconvolve_params_name": "deconvolve_0"})
     m.PreprocessParams.insert1({"preprocess_params_name": "preprocess_0"})
     m.PreprocessParams.insert1({"preprocess_params_name": "preprocess_1"})
